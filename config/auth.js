6959675a2aa66de6571bfc3abb2bf4a5ae6c5cc3
Ferit Topcu <f_topcu@live.de>
2014-01-22 20:51:52 +0100

    have to change fb callback uri

--- config/auth.js
+++ config/auth.js
@@ -6,7 +6,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '260747077418568', // your App ID
 		'clientSecret' 	: 'd68331691e468fa43df4a7797253cd56', // your App Secret
-		'callbackURL' 	: 'http://localhost:7070/auth/facebook/callback'
+		'callbackURL' 	: 'http://nodeauth.azurewebsites.net/auth/facebook/callback'
 	},
 
 	'twitterAuth' : {
