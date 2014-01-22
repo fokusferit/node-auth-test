24faaedba9b230a89beee7b6ed52c6bb9af1e45c
Ferit Topcu <f_topcu@live.de>
2014-01-22 21:19:49 +0100

    fix: I forgot the [] marks for the scope

--- app/routes.js
+++ app/routes.js
@@ -45,7 +45,7 @@ module.exports = function(app, passport) {
 	// FACEBOOK ROUTES =====================
 	// =====================================
 	// route for facebook authentication and login
-	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email', 'read_insights' }));
+	app.get('/auth/facebook', passport.authenticate('facebook', { scope : ['email', 'read_insights'] }));
 
 	// handle the callback after facebook has authenticated the user
 	app.get('/auth/facebook/callback',
