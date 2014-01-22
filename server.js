c47a25496cd714497a1e92f75d7e0253c7ee3b56
Ferit Topcu <f_topcu@live.de>
2014-01-22 20:48:29 +0100

    changing db entry direct

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // get all the tools we need
 var express  = require('express');
 var app      = express();
-var port     = process.env.PORT || 1337;
+var port     = process.env.PORT || 7070;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash 	 = require('connect-flash');
