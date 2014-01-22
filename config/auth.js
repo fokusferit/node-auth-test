1a1c1b35b0a0ee898bbabcaa03a2877c20394c6f
Ferit Topcu <f_topcu@live.de>
2014-01-22 19:50:39 +0100

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,24 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '260747077418568', // your App ID
+		'clientSecret' 	: 'd68331691e468fa43df4a7797253cd56', // your App Secret
+		'callbackURL' 	: 'http://localhost:7070/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+
+};
